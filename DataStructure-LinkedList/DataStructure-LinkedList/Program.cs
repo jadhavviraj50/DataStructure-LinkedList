d993dc3d7e5e1520598ab6bad14338da54512466
Viraj <jadhavviraj50@gmail.com>
2022-05-10 23:29:29 +0530

    [Viraj]-Added UC8-Insert40After30 file

--- DataStructure-LinkedList/DataStructure-LinkedList/Program.cs
+++ DataStructure-LinkedList/DataStructure-LinkedList/Program.cs
@@ -8,7 +8,7 @@ namespace LinkedListProblem
         {
             Console.WriteLine("-----------Welcome to Linked List Program----------");
             bool end = true;
-            Console.WriteLine("Enter Option\n1.Add Data\n2.Reverse Data\n3.Insert Data\n4.RemoveFirstNode\n5.RemoveLastElement\n6.Search Node\n7.End Of Program ");
+            Console.WriteLine("Enter Option\n1.Add Data\n2.Reverse Data\n3.Insert Data\n4.RemoveFirstNode\n5.RemoveLastElement\n6.Search Node\n7.InsertNewData\n8.End Of Program ");
             while (end)
             {
                 LinkedList list = new LinkedList();
@@ -55,8 +55,15 @@ namespace LinkedListProblem
                         list.Add(70);
                         list.Search(30);
                         break;
-
                     case 7:
+                        list.Add(56);
+                        list.Add(30);
+                        list.Add(70);
+                        int newNode = list.Search(30);
+                        list.InsertAtParticularPosition(newNode + 1, 40);
+                        list.Display();
+                        break;
+                    case 8:
                         end = false;
                         Console.WriteLine("Program Is Ended.");
                         break;
