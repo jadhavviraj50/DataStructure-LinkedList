80eb6c4b259ba84f5c99f9bcec4d49adb233411d
Viraj <jadhavviraj50@gmail.com>
2022-05-10 21:44:44 +0530

    [Viraj]-Added UC4-Insert30Between56&70 file

--- DataStructure-LinkedList/DataStructure-LinkedList/Program.cs
+++ DataStructure-LinkedList/DataStructure-LinkedList/Program.cs
@@ -9,10 +9,13 @@ namespace LinkedListProblem
             Console.WriteLine("-----Welcome to Linked List Program-----");
             LinkedList list = new LinkedList();
             list.Add(56);
-            list.Add(30);
+            //list.Add(30);
             list.Add(70);
-            list.Display();
-            list.AddInReverseOrder(70);
+            //list.Display();
+            //list.AddInReverseOrder(70);
+            //list.Display();
+            int num = list.Search(56);
+            list.InsertAtParticularPosition(num + 1, 30);
             list.Display();
 
         }
